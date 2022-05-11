429ae5450bf1df71ddd758ad262373ea00959325
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-05-11 17:30:31 +0600

    validParentheses done

---
+++ src/main/java/validParentheses/Solution.java
@@ -0,0 +1,20 @@
+package validParentheses;
+
+public class Solution {
+    public static boolean validParentheses(String parens) {
+        int count = 0;
+        char[] charArray = new char[parens.length()];
+        for (int i = 0; i < parens.length(); i++) {
+            if (parens.charAt(i) == '(') {
+                count++;
+            } else if (parens.charAt(i) == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        if (count == 0) return true;
+        else return false;
+    }
+}
